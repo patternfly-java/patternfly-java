7f5010826436c2863a2c42cf613c96d364e59600
Harald Pehl <harald.pehl@gmail.com>
2025-05-05 16:48:06 +0200

    Replace TypeScript with JavaScript for token generation

--- tokens/src/scripts/generate.mjs
+++ tokens/src/scripts/generate.mjs
@@ -1,40 +1,32 @@
-///
-///  Copyright 2023 Red Hat
-///
-///  Licensed under the Apache License, Version 2.0 (the "License");
-///  you may not use this file except in compliance with the License.
-///  You may obtain a copy of the License at
-///
-///      https://www.apache.org/licenses/LICENSE-2.0
-///
-///  Unless required by applicable law or agreed to in writing, software
-///  distributed under the License is distributed on an "AS IS" BASIS,
-///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-///  See the License for the specific language governing permissions and
-///  limitations under the License.
-///
-
-// @ts-ignore
+/*
+ *  Copyright 2023 Red Hat
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 import {writeFile} from 'node:fs/promises';
 import camelCase from 'camelcase';
 import * as TOKENS from "@patternfly/react-tokens";
 
 // ------------------------------------------------------ constants
 
-const keywords: string[] = ["clone", "import", "package", "private"];
-const root: string = "../main/java";
-const path: string = "org/patternfly/token";
-const package_: string = path.replaceAll('/', '.');
+const keywords = ["clone", "import", "package", "private"];
+const root = "../main/java";
+const path = "org/patternfly/token";
+const package_ = path.replaceAll('/', '.');
 
 // ------------------------------------------------------ tokens
 
-interface Token {
-    name: string;
-    value: string;
-    var: string;
-}
-
-const generateTokens = (entries: [string, Token][]) => `package ${package_};
+const generateTokens = (entries) => `package ${package_};
 
 import javax.annotation.processing.Generated;
 
@@ -65,14 +57,14 @@ public enum Token {
 }
 `;
 
-const generateConstant = (javaName: string, token: Token) =>
+const generateConstant = (javaName, token) =>
     `${javaName}("${token.name}", "${token.value}", "${token.var}"),`;
 
-const failSafeName = (value: string) => keywords.includes(value) ? value + "_" : value
+const failSafeName = (value) => keywords.includes(value) ? value + "_" : value
 
 // ------------------------------------------------------ main
 
-let tokens: [string, Token][] = Object.keys(TOKENS)
+let tokens = Object.keys(TOKENS)
     .filter(key => key.startsWith("t_"))
     .map(key => {
         let javaName = failSafeName(camelCase(key.substring(2)));
@@ -81,7 +73,7 @@ let tokens: [string, Token][] = Object.keys(TOKENS)
             .replace(/\n/g, "\\n")
             .replace(/"/g, '\\"');
         // @ts-ignore
-        let token: Token = {name: TOKENS[key].name, value: safeValue, var: TOKENS[key].var};
+        let token = {name: TOKENS[key].name, value: safeValue, var: TOKENS[key].var};
         return [javaName, token];
     });
 
